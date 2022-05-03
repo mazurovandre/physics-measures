739a2621f5fed32a708a53f5d4716341b35494d5
Andrey <mazurovandre@gmail.com>
2022-05-03 16:53:29 +0300

    First commit

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// import { createApp } from 'vue'
+// import App from './App.vue'
+//
+// createApp(App).mount('#app')
 
-createApp(App).mount('#app')
+
+import { createApp } from 'vue';
+import Antd from 'ant-design-vue';
+import App from './App.vue';
+import 'ant-design-vue/dist/antd.css';
+
+const app = createApp(App);
+
+app.use(Antd).mount('#app');
